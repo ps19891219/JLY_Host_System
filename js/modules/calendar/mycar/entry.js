dc0ad7e0d0df72690046fe3a968774aca1de4c60
ps19891219 <ps19891219@gmail.com>
2026-09-14 05:31:37 +0800

    Bump MyCar sync cache version

--- js/modules/calendar/mycar/entry.js
+++ js/modules/calendar/mycar/entry.js
@@ -1,12 +1,9 @@
 (function () {
   "use strict";
-
-  const marker = "__JLYMyCarCalendarEntryLoading";
-  if (window[marker]) return;
-  window[marker] = true;
-
+  if (window.__JLYMyCarCalendarEntryLoading) return;
+  window.__JLYMyCarCalendarEntryLoading = true;
   const script = document.createElement("script");
-  script.src = "/js/modules/calendar/mycar/sync.js?v=2";
+  script.src = "/js/modules/calendar/mycar/sync.js?v=3";
   script.async = false;
   document.head.appendChild(script);
 })();
