33d368ab6780151ac2c90adf9f7631b0e5b15db9
ps19891219 <ps19891219@gmail.com>
2026-09-14 05:31:51 +0800

    Bump isolated MyCar Calendar entry cache

--- js/modules/calendar/mycar-calendar-repair.js
+++ js/modules/calendar/mycar-calendar-repair.js
@@ -6,7 +6,7 @@
   batch.async = false;
   batch.onload = function () {
     const entry = document.createElement("script");
-    entry.src = "/js/modules/calendar/mycar/entry.js?v=2";
+    entry.src = "/js/modules/calendar/mycar/entry.js?v=3";
     entry.async = false;
     document.head.appendChild(entry);
   };
